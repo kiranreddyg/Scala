238ebdab65c94c8284c67f147394da23c1d3bf85
kiranreddyg <kiran.reddyg@gmail.com>
2016-04-27 06:31:08 -0400

    First Version

--- HelloFirst/src/com/itandfs/scala/Runner.scala
+++ HelloFirst/src/com/itandfs/scala/Runner.scala
@@ -6,5 +6,27 @@ package com.itandfs.scala
 object Runner {
   def main(args: Array[String]) {
     println("Hello First App ...");
+    var str = "hello World";
+    println(str.length)
+    str.filter(_ != 'l').foreach(println);
+    for(c <- str) println(c);
+    println(str.toUpperCase)
+
+    str =
+      """Hello 123
+        |adfafafa
+        |dafafa
+        |dafafadfa
+      """.stripMargin;
+    println(str);
+    val regex = "[0-9]+".r;
+    var address = "3710 Broadrun Dr,fairfax, va-22033";
+    var housenumber = regex.findFirstIn(address);
+    housenumber.foreach(println)
+    var housearray = housenumber.toArray
+    println(housearray(0))
+    val pattern = "([0-9]+) ([A-Za-z]+) ([A-Za-z]+)".r
+    val pattern(count, fruit1, fruit2) = "100 Bananas apples"
+    println(fruit2)
   }
 }
